f66ff0d3e7e1204adce17690bcee653b3ace599c
Tiago Hermano <tiagofhermano@gmail.com>
2019-06-16 19:22:14 -0300

    final tweak on ex2.sh

--- trabalho/ex1.sh
+++ trabalho/ex1.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 clear
-echo "Informe a taxa de juros (i): "
+echo "Informe a taxa de juros (i):"
 read taxaJuros
-echo "Informe o valor do deposito mensal (R): "
+echo "Informe o valor do deposito mensal (R):"
 read depositoMensal
-echo "Informe o número de meses (n): "
+echo "Informe o número de meses (n):"
 read nMeses
+clear
+
+
 
 valorFuturo=$(echo "$depositoMensal * (((1+$taxaJuros)^$nMeses-1)/$taxaJuros)" | bc -l)
-
-printf "Valor Futuro do Investimento (S): %0.2f\n" "$valorFuturo"
+printf "Taxa de Juros (i).....................: %0.2f\n" "$taxaJuros"
+printf "Deposito Mensal (R)...................: %0.2f\n" "$depositoMensal"
+printf "Número de meses (n)...................: %i\n" "$nMeses"
+printf "Valor Futuro do Investimento (S)......: %0.2f\n" "$valorFuturo"
 exit 0
